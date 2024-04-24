28332c36cb1662c0e230ee4d5ab37d74ffd582cf
MikeDesktop <micheledong2803@gmail.com>
2024-04-24 16:51:50 +0200

    m3 - createElement

--- main.js
+++ main.js
@@ -4,13 +4,16 @@ const lista = [
     "olio",
     "pasta"
 ];
+const classeLista = document.querySelector("ol");
 
 // for (let i = 0; i < lista.length; i++) {
 //     console.log(lista[i])
 // }
 let i = 0;
 while( i < lista.length) {
-    console.log(lista[i]);
+    const element = document.createElement("li");
+    element.append(lista[i]);
+    classeLista.append(element);
     i++;
 }
 
